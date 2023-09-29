dc110d314940c1716c7527963cf2cda48f60a0e6
sajjad-njr <maxsajjad29@gmail.com>
2023-09-29 20:41:13 +0600

    base repo clean

--- live_7/android/app/src/main/kotlin/com/example/live_7/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.live_7
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mod_7/android/app/src/main/kotlin/com/example/mod_7/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mod_7
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
