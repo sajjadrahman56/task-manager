8db69f373095d5fe69bc118b0e6997f5ce9e395d
sajjad-njr <maxsajjad29@gmail.com>
2023-10-12 21:44:12 +0600

    repo clean

--- assign_09/android/app/src/main/kotlin/com/example/assign_09/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.assign_09
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- live_09/android/app/src/main/kotlin/com/example/live_09/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.live_09
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
