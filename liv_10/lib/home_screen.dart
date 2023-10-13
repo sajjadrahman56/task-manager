6089d9dd9380c6466cdb716a8697023c829b3a3e
sajjad-njr <maxsajjad29@gmail.com>
2023-10-13 23:18:09 +0600

    functional added

--- liv_10/lib/home_screen.dart
+++ liv_10/lib/home_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:liv_10/add_new_task.dart';
-import 'package:liv_10/todo.dart';
-import 'package:intl/intl.dart';
-import 'package:liv_10/update_task.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,119 +8,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Todo> todoList = [];
+  final List<int> _selectedIndices = [];
+  final List<String> items = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
+    'Item 7',
+    'Item 8'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todos'),
+        title: Text('Selection Screen'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showModalBottomSheet(
+          showDialog(
             context: context,
-            builder: (context) {
-              return AddNewTaskModal(
-                onAddTap: (Todo task) {
-                  addTodo(task);
-                },
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Selected Items'),
+                content:
+                    Text('No of selected items ${_selectedIndices.length} '),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Close')),
+                ],
               );
             },
           );
         },
-        child: const Icon(Icons.add),
+        child: Icon(Icons.check),
       ),
-      body: ListView.separated(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          final Todo todo = todoList[index];
-          final String formattedDate =
-              DateFormat('hh:mm a dd-MM-yy').format(todo.createdDateTime);
-          return ListTile(
-            tileColor: todo.status == 'done' ? Colors.grey : null, // ternary
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
             onTap: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: const Text('Actions'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ListTile(
-                            leading: const Icon(Icons.edit),
-                            title: const Text('Update'),
-                            onTap: () {
-                              Navigator.pop(context);
-                              showModalBottomSheet(
-                                  context: context,
-                                  builder: (context) {
-                                    return UpdateTaskModal(
-                                      todo: todo,
-                                      onTodoUpdate:
-                                          (String updatedDetailsText) {
-                                        updateTodo(index, updatedDetailsText);
-                                      },
-                                    );
-                                  });
-                            },
-                          ),
-                          const Divider(
-                            height: 0,
-                          ),
-                          ListTile(
-                            leading: const Icon(Icons.delete_outline),
-                            title: const Text('Delete'),
-                            onTap: () {
-                              deleteTodo(index);
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      ),
-                    );
-                  });
+              setState(() {
+                if (_selectedIndices.contains(index)) {
+                  _selectedIndices.remove(index);
+                } else {
+                  _selectedIndices.add(index);
+                }
+              });
             },
-            onLongPress: () {
-              String currentStatus =
-                  todo.status == 'pending' ? 'done' : 'pending';
-              updateTodoStatus(index, currentStatus);
-            },
-            leading: CircleAvatar(
-              child: Text('${index + 1}'),
+            child: Container(
+              color: _selectedIndices.contains(index) ? Colors.blue : null,
+              child: ListTile(
+                title: Text(items[index]),
+              ),
             ),
-            title: Text(todo.details),
-            subtitle: Text(formattedDate),
-            trailing: Text(todo.status.toUpperCase()),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider(
-            height: 4,
           );
         },
       ),
     );
-  }
-
-  void addTodo(Todo todo) {
-    todoList.add(todo);
-    setState(() {});
-  }
-
-  void deleteTodo(int index) {
-    todoList.removeAt(index);
-    setState(() {});
-  }
-
-  void updateTodo(int index, String todoDetails) {
-    todoList[index].details = todoDetails;
-    setState(() {});
-  }
-
-  void updateTodoStatus(int index, String status) {
-    todoList[index].status = status;
-    setState(() {});
   }
 }
--- liv_10/lib/main.dart
+++ liv_10/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:liv_10/home_screen.dart';
 
-/// Todo_Application
-/// CRUD - Create, Read, Update, Delete
-
 void main() {
   runApp(const TodoApp());
 }
