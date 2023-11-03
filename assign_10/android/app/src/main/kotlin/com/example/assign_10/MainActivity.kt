fbd6b832825b2c417f41e92644fefc83d7896145
sajjadrahman56 <maxsajjad29@gmail.com>
2023-11-03 22:48:57 +0600

    old project removed

--- assign_10/android/app/src/main/kotlin/com/example/assign_10/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.assign_10
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- liv_10/android/app/src/main/kotlin/com/example/liv_10/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.liv_10
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
